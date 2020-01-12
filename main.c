0e4df19192d90d0a1f3d121f2137be326623e0fe
Raphiy <rfroemberg@gmail.com>
2020-01-12 22:47:51 +0100

    update

--- main.c
+++ main.c
@@ -7,10 +7,10 @@
 
 #include <stdio.h>
 
-int main(){
+int main() {
 	FILE *doc;
 	doc = fopen("weihnachten.txt", "rb");
-	if(doc == NULL){
+	if (doc == NULL) {
 		printf("Unzulässiger Text");
 	}
 	int binchar[8] = {0};
@@ -18,31 +18,32 @@ int main(){
 	int input;
 	int n = 0;
 
-	while(1){
+	while (1) {
 		input = fgetc(doc);
-		if (!(input == 49 || input == 48 || input ==32 || input ==10 || input ==13 || input ==EOF)){
+		if (input == 49 || input == 48 || input == 32 || input == 10|| input == 13 || input == EOF) {
+			if (input == 49 || input == 48) {
+				binchar[n] = input - 48;
+				n++;
+			}
+		} else {
+			printf("\nProgrammabbruch: Ungültiges Zeichen.");
 			break;
 		}
 
-		if (!(input == 32 || input == 10 || input ==13 || input == EOF)){
-			binchar[n] = input -48;
-			n++;
-		}
-
-		if (n == 8){
+		if (n == 8) {
 			n = 128;
-			for (int i = 0; i < 8; i++){
+			for (int i = 0; i < 8; i++) {
 				output += binchar[i] * n;
-				n = n/2;
+				n = n / 2;
 			}
 			printf("%c", output);
 			output = 0;
 			n = 0;
 		}
-		if (input == EOF){
-			if(n!=0){
+		if (input == EOF) {
+			if (n != 0) {
 				printf("\nEs sind noch folgende Zeichen übrig:");
-				for (int x = 0; x <= n; x++){
+				for (int x = 0; x <= n; x++) {
 					printf("%d", binchar[x]);
 				}
 			}
